9dbe9126f40d7290433c307985e57a4a1d03d6db
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-04-03 11:37:37 -0400

    initial commit!

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req,res) => {
+    console.log('test route');
+    res.send('<h1>hello world</h1>');
+})
+
+
+app.listen(8080, console.log('server is listening on port 8080'));
+
+
