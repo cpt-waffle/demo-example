a223a5ba2897a78312ede8590281f564321a519d
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-04-03 12:02:18 -0400

    Added login route

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const express = require('express');
 const app = express();
 
 
+
+
+
 app.get('/', (req,res) => {
     console.log('test route');
     res.send('<h1>hello world</h1>');
 })
+
+
+app.get('/login', (req,res) => {
+    console.log('test route');
+    res.send('<h1>hello world</h1>');
+})
+
 
 
 app.listen(8080, console.log('server is listening on port 8080'));
